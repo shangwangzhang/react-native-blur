84dc4e8c4c22be2e611fbddf3c742d1061075241
ShangWang Zhang <shangwangzhang@gamil.com>
2021-09-29 02:27:43 -0400

    Updated

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 import {
   View,
   Platform,
-  NativeModules,
   requireNativeComponent,
   StyleSheet,
-  TouchableWithoutFeedback,
-  Animated,
 } from 'react-native'
 import React from 'react'
 import PropTypes from 'prop-types'
-
-const emitter = require('tiny-emitter/instance')
 
 const iface = {
   name: 'BlurView',
@@ -26,100 +21,28 @@ const iface = {
 }
 
 const RCTBlurOverlay = Platform.select({
-  ios: () => requireNativeComponent('BlurOverlay', iface),
-  android: () => requireNativeComponent('RCTBlurOverlay', iface),
+  ios: () => requireNativeComponent('BlurOverlay'),
+  android: () => requireNativeComponent('RCTBlurOverlay'),
 })()
 
-export default class BlurOverlay extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      showBlurOverlay: false,
-      fadeIn: new Animated.Value(0),
-    }
-    this._openOverlay = this.openOverlay.bind(this)
-    this._closeOverlay = this.closeOverlay.bind(this)
-  }
-
-  openOverlay() {
-    this.setState(
-      {
-        showBlurOverlay: true,
-        fadeIn: new Animated.Value(0),
-      },
-      () => {
-        Animated.timing(this.state.fadeIn, {
-          toValue: 1,
-          duration: 500,
-          useNativeDriver: true,
-        }).start()
-      }
-    )
-  }
-
-  closeOverlay() {
-    Animated.timing(this.state.fadeIn, {
-      toValue: 0,
-      duration: 500,
-      useNativeDriver: true,
-    }).start(() => this.setState({ showBlurOverlay: false }))
-  }
-
-  componentDidMount() {
-    emitter.on('drawer-open' + this.props.idBlur, this._openOverlay)
-    emitter.on('drawer-close' + this.props.idBlur, this._closeOverlay)
-  }
-
-  componentWillUnmount() {
-    emitter.off('drawer-open' + this.props.idBlur, this._openOverlay)
-    emitter.off('drawer-close' + this.props.idBlur, this._closeOverlay)
-  }
+export default class BlurOverlayComponent extends React.Component {
 
   render() {
-    const { children } = this.props
-
-    if (!this.state.showBlurOverlay) {
-      return null;
-    }
+    const { children, style } = this.props
 
     return (
-      <Animated.View style={[{ opacity: this.state.fadeIn }, styles.container]}>
-        <TouchableWithoutFeedback
-          style={styles.container}
-          onPress={this.props.onPress}
-        >
-          <RCTBlurOverlay
-            {...this.props}
-            style={[this.props.customStyles, styles.container]}
-          >
-            <View style={[this.props.customStyles, styles.container]}>
-              {children}
-            </View>
-          </RCTBlurOverlay>
-        </TouchableWithoutFeedback>
-      </Animated.View>
+      <RCTBlurOverlay
+        {...this.props}
+        style={[styles.container, style]}
+      >
+        {children}
+      </RCTBlurOverlay>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
-    flex: 1,
-    left: 0,
-    top: 0,
-    bottom: 0,
-    right: 0,
-    width: null,
-    height: null,
-    zIndex: 999,
+    ...StyleSheet.absoluteFillObject,
   },
 })
-
-export function openOverlay(idBlur) {
-  emitter.emit('drawer-open' + idBlur)
-}
-
-export function closeOverlay(idBlur) {
-  emitter.emit('drawer-close' + idBlur)
-}
